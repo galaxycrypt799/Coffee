2d29e764b66e67b1a0faf0ea06df7f71a44f9ff6
Kim Ngan <ngandinhkim262@gmail.com>
2026-05-11 21:09:42 +0700

    Implement order management bloc for admin

--- coffee_admin/lib/src/modules/operations/views/orders_event.dart
+++ coffee_admin/lib/src/modules/operations/views/orders_event.dart
@@ -1,2 +1,24 @@
-// TODO: Implement by team member
-// File: src\modules\operations\views\orders_event.dart
+part of 'orders_bloc.dart';
+
+abstract class OrdersEvent extends Equatable {
+  const OrdersEvent();
+  @override
+  List<Object> get props => [];
+}
+
+class GetOrders extends OrdersEvent {
+  final bool showLoader;
+
+  const GetOrders({this.showLoader = true});
+
+  @override
+  List<Object> get props => [showLoader];
+}
+
+class UpdateOrderStatus extends OrdersEvent {
+  final String orderId;
+  final String status;
+  const UpdateOrderStatus(this.orderId, this.status);
+  @override
+  List<Object> get props => [orderId, status];
+}
