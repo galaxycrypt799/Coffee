e8fae61606873c69f709670a2386c5763b0756e6
Kim Ngan <ngandinhkim262@gmail.com>
2026-05-11 21:12:35 +0700

    Define order repository interface

--- coffee_admin/lib/src/modules/operations/views/order_repo.dart
+++ coffee_admin/lib/src/modules/operations/views/order_repo.dart
@@ -1,2 +1,9 @@
-// TODO: Implement by team member
-// File: src\modules\operations\views\order_repo.dart
+import 'order.dart';
+
+abstract class OrderRepo {
+  Future<void> createOrder(Order order);
+  Future<List<Order>> getMyOrders(String userId);
+  Future<List<Order>> getOrders();
+  Future<void> updateOrderStatus(String orderId, String status);
+  Future<Map<String, dynamic>> getRevenueStats();
+}
