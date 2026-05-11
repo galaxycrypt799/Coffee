079c7ebc793ce7dd59c5c743bcc0b9cd5c91aa10
Vuong <Vuong7411@gmail.com>
2026-05-11 21:20:28 +0700

    Implement revenue management bloc

--- coffee_admin/lib/src/modules/operations/views/revenue_event.dart
+++ coffee_admin/lib/src/modules/operations/views/revenue_event.dart
@@ -1,2 +1,10 @@
-// TODO: Implement by team member
-// File: src\modules\operations\views\revenue_event.dart
+part of 'revenue_bloc.dart';
+
+sealed class RevenueEvent extends Equatable {
+  const RevenueEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class GetRevenueRequested extends RevenueEvent {}
