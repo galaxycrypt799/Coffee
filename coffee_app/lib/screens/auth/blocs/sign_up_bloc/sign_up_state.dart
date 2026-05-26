b2ccedfbcc5a6ca4e48511e147779f05399ee219
Nguyen Vuong <Vuong7411@gmail.com>
2026-05-26 22:33:02 +0700

    Update customer sign in and sign up blocs

--- coffee_app/lib/screens/auth/blocs/sign_up_bloc/sign_up_state.dart
+++ coffee_app/lib/screens/auth/blocs/sign_up_bloc/sign_up_state.dart
@@ -1,2 +1,23 @@
-// TODO: Implement by team member
-// File: screens\auth\blocs\sign_up_bloc\sign_up_state.dart
+part of 'sign_up_bloc.dart';
+
+sealed class SignUpState extends Equatable {
+  const SignUpState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class SignUpInitial extends SignUpState {}
+
+class SignUpSuccess extends SignUpState {}
+
+class SignUpFailure extends SignUpState {
+  const SignUpFailure(this.message);
+
+  final String message;
+
+  @override
+  List<Object> get props => [message];
+}
+
+class SignUpProcess extends SignUpState {}
