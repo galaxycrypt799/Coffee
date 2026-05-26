b9518cfa17914f6faf25d38437670a0af62716e0
Minh Toan <minhtoan17022004@icloud.com>
2026-05-26 22:30:25 +0700

    Implement create coffee management bloc

--- coffee_admin/lib/src/modules/operations/blocs/create_coffee_bloc/create_coffee_bloc.dart
+++ coffee_admin/lib/src/modules/operations/blocs/create_coffee_bloc/create_coffee_bloc.dart
@@ -1,2 +1,42 @@
-// TODO: Implement by team member
-// File: src\modules\operations\blocs\create_coffee_bloc\create_coffee_bloc.dart
+import 'package:bloc/bloc.dart';
+import 'package:coffee_repository/coffee_repository.dart';
+import 'package:equatable/equatable.dart';
+
+part 'create_coffee_event.dart';
+part 'create_coffee_state.dart';
+
+class CreateCoffeeBloc extends Bloc<CreateCoffeeEvent, CreateCoffeeState> {
+  CreateCoffeeBloc(this._coffeeRepo) : super(CreateCoffeeInitial()) {
+    on<CreateCoffeeRequested>((event, emit) async {
+      emit(CreateCoffeeLoading());
+      try {
+        await _coffeeRepo.createCoffee(event.coffee);
+        emit(const CreateCoffeeSuccess('Đã thêm món vào menu'));
+      } catch (error) {
+        emit(CreateCoffeeFailure(error.toString()));
+      }
+    });
+
+    on<UpdateCoffeeRequested>((event, emit) async {
+      emit(CreateCoffeeLoading());
+      try {
+        await _coffeeRepo.updateCoffee(event.coffee);
+        emit(const CreateCoffeeSuccess('Đã cập nhật món'));
+      } catch (error) {
+        emit(CreateCoffeeFailure(error.toString()));
+      }
+    });
+
+    on<DeleteCoffeeRequested>((event, emit) async {
+      emit(CreateCoffeeLoading());
+      try {
+        await _coffeeRepo.deleteCoffee(event.coffeeId);
+        emit(const CreateCoffeeSuccess('Đã xoá món khỏi menu'));
+      } catch (error) {
+        emit(CreateCoffeeFailure(error.toString()));
+      }
+    });
+  }
+
+  final CoffeeRepo _coffeeRepo;
+}
--- coffee_admin/lib/src/modules/operations/blocs/create_coffee_bloc/create_coffee_state.dart
+++ coffee_admin/lib/src/modules/operations/blocs/create_coffee_bloc/create_coffee_state.dart
@@ -1,2 +1,30 @@
-// TODO: Implement by team member
-// File: src\modules\operations\blocs\create_coffee_bloc\create_coffee_state.dart
+part of 'create_coffee_bloc.dart';
+
+sealed class CreateCoffeeState extends Equatable {
+  const CreateCoffeeState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class CreateCoffeeInitial extends CreateCoffeeState {}
+
+final class CreateCoffeeFailure extends CreateCoffeeState {
+  const CreateCoffeeFailure([this.message = '']);
+
+  final String message;
+
+  @override
+  List<Object> get props => [message];
+}
+
+final class CreateCoffeeLoading extends CreateCoffeeState {}
+
+final class CreateCoffeeSuccess extends CreateCoffeeState {
+  const CreateCoffeeSuccess([this.message = '']);
+
+  final String message;
+
+  @override
+  List<Object> get props => [message];
+}
